82acb59e3650b84c6a682f1d67321584ebb8f0ae
darshan <darshan@darshans-MacBook-Air.local>
2025-08-20 07:36:16 +0530

    Deploy to GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -10,34 +9,33 @@ import Faculty from './components/Faculty';
 import Admissions from './components/Admissions';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import Research from './components/research'
+import Research from './components/research';
+
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-white">
         <Header />
         <Routes>
-          <Route path="/" element={
-            <>
-              <Hero />
-              <About />
-            
-              <Facilities />
-              <StudentLife />
-          
-              <Admissions />
-              <Contact />
-            </>
-          } />
-        
-          <Route path="/faculty" element={<Faculty />} />
-           <Route path="/program" element={<Programs />} />
-           <Route path='/research' element={  <Research/>}/>
-        </Routes>
-        
-        <Footer />
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <About />
+                <Facilities />
+                <StudentLife />
+                <Admissions />
+                <Contact />
+              </>
+            }
+          />
 
-        
+          <Route path="/faculty" element={<Faculty />} />
+          <Route path="/program" element={<Programs />} />
+          <Route path="/research" element={<Research />} />
+        </Routes>
+        <Footer />
       </div>
     </Router>
   );
